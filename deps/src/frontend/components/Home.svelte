54a8c3ab1ad675286eaa8bdd6f26dc652929f504
Mister Burgess <matthewburgess@Matthews-Mac-mini.local>
2023-12-16 14:43:07 -0500

    resetting to 0

---
+++ deps/src/frontend/components/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+  import logo from ".././assets/camp_logo.png"
+</script>
+
+<div>
+  <header class="App-header">
+    <p class="slogan">Motoko Bootcamp Dao</p>
+    <p class="">Welcome to your Super DAO!</p>
+  </header>
+</div>
